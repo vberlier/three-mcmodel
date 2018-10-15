067eaf1855944a4f079158a09e439267d83c1d67
Valentin Berlier <berlier.v@gmail.com>
2018-10-16 01:45:00 +0200

    Export the geometry class and the model interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export { MinecraftModelGeometry } from './geometry'
 export { MinecraftModelMesh } from './mesh'
+export {
+  ArrayVector3, isArrayVector3,
+  MinecraftModelElement, isMinecraftModelElement,
+  MinecraftModel, isMinecraftModel
+} from './model'
