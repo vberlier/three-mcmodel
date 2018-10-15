8b77a68fd2a887bb386b3397dc7b5327146b1c01
Valentin Berlier <berlier.v@gmail.com>
2018-10-15 21:19:39 +0200

    Add comments

--- examples/webpack-example/src/index.js
+++ examples/webpack-example/src/index.js
@@ -1,14 +1,18 @@
 import { Scene, PerspectiveCamera, WebGLRenderer } from 'three'
 
+// Create the scene and the camera
 const scene = new Scene()
 const camera = new PerspectiveCamera()
 
+// Create the renderer and append it to the document body
 const renderer = new WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+// Render the scene
 renderer.render(scene, camera)
 
+// Update the dimensions of the viewport when the window gets resized
 window.addEventListener('resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
