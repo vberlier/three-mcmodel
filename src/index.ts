8266adeb7d361f518244767479af36b894474ea7
Valentin Berlier <berlier.v@gmail.com>
2018-10-17 00:43:15 +0200

    Include faces in model interface

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { MinecraftModelGeometry } from './geometry'
 export { MinecraftModelMesh } from './mesh'
 export {
   ArrayVector3, isArrayVector3,
+  MinecraftModelFace, isMinecraftModelFace,
   MinecraftModelElement, isMinecraftModelElement,
   MinecraftModel, isMinecraftModel
 } from './model'
--- src/model.ts
+++ src/model.ts
@@ -8,16 +8,42 @@ export function isArrayVector3 (arrayVector: any): arrayVector is ArrayVector3 {
   )
 }
 
+export interface MinecraftModelFace {
+  texture: string
+}
+
+export function isMinecraftModelFace (face: any): face is MinecraftModelFace {
+  return (
+    face &&
+    typeof face.texture === 'string'
+  )
+}
+
 export interface MinecraftModelElement {
   from: ArrayVector3
   to: ArrayVector3
+  faces: {
+    down?: MinecraftModelFace,
+    up?: MinecraftModelFace,
+    north?: MinecraftModelFace,
+    south?: MinecraftModelFace,
+    west?: MinecraftModelFace,
+    east?: MinecraftModelFace
+  }
 }
 
 export function isMinecraftModelElement (element: any): element is MinecraftModelElement {
   return (
     element &&
     isArrayVector3(element.from) &&
-    isArrayVector3(element.to)
+    isArrayVector3(element.to) &&
+    element.faces &&
+    (element.faces.down === undefined || isMinecraftModelFace(element.faces.down)) &&
+    (element.faces.up === undefined || isMinecraftModelFace(element.faces.up)) &&
+    (element.faces.north === undefined || isMinecraftModelFace(element.faces.north)) &&
+    (element.faces.south === undefined || isMinecraftModelFace(element.faces.south)) &&
+    (element.faces.west === undefined || isMinecraftModelFace(element.faces.west)) &&
+    (element.faces.east === undefined || isMinecraftModelFace(element.faces.east))
   )
 }
 
