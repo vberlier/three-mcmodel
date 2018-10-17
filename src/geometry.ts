31e3422e54958e372207954cef9a1fa9c6b19d0d
Valentin Berlier <berlier.v@gmail.com>
2018-10-17 22:02:43 +0200

    Handle explicit uvs

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,6 +1,6 @@
 import { BufferGeometry, Float32BufferAttribute, Uint16BufferAttribute } from 'three'
 
-import { MinecraftModel, MinecraftModelFaceName } from './model'
+import { MinecraftModel, MinecraftModelFaceName, ArrayVector3, ArrayVector4} from './model'
 
 const faceVertices = {
   west: [[0, 0, 0], [0, 1, 0], [0, 1, 1], [0, 0, 1]],
@@ -29,22 +29,34 @@ export class MinecraftModelGeometry extends BufferGeometry {
     for (const element of model.elements) {
       const { from, to } = element
 
-      for (const faceName in element.faces) {
+      for (const name in element.faces) {
+        const faceName = name as MinecraftModelFaceName
+        const face = element.faces[faceName]
+
         const i = vertices.length / 3
         indices.push(i, i + 2, i + 1)
         indices.push(i, i + 3, i + 2)
 
-        for (const vertex of faceVertices[faceName as MinecraftModelFaceName]) {
+        for (const vertex of faceVertices[faceName]) {
           vertices.push(...vertex.map((v, i) => v === 0 ? from[i] : to[i]))
         }
 
-        uvs.push(0, 0)
-        uvs.push(0, 1)
-        uvs.push(1, 1)
-        uvs.push(1, 0)
+        const [u1, v1, u2, v2] = (face.uv || this.defaultUvs(faceName, from, to)).map(coordinate => coordinate / 16)
+
+        const bias = 1 - Math.max(v1, v2)
+        const [fixedV1, fixedV2] = [v1, v2].map(coordinate => bias + coordinate)
+
+        uvs.push(u1, fixedV1)
+        uvs.push(u1, fixedV2)
+        uvs.push(u2, fixedV2)
+        uvs.push(u2, fixedV1)
       }
     }
 
     return [vertices, uvs, indices]
+  }
+
+  defaultUvs (faceName: MinecraftModelFaceName, from: ArrayVector3, to: ArrayVector3): ArrayVector4 {
+    return [0, 0, 16, 16]
   }
 }
