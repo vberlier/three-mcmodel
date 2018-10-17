87621abfa8346c478bf530e7228a4781a34dd35b
Valentin Berlier <berlier.v@gmail.com>
2018-10-17 02:09:02 +0200

    Only display the included faces

--- src/geometry.ts
+++ src/geometry.ts
@@ -1,6 +1,6 @@
 import { BufferGeometry, Float32BufferAttribute } from 'three'
 
-import { MinecraftModel } from './model'
+import { MinecraftModel, MinecraftModelFaceName } from './model'
 
 export class MinecraftModelGeometry extends BufferGeometry {
   constructor (model: MinecraftModel) {
@@ -11,16 +11,18 @@ export class MinecraftModelGeometry extends BufferGeometry {
     for (const element of model.elements) {
       const { from: [x1, y1, z1], to: [x2, y2, z2] } = element
 
-      const faces = [
-        [x1, y2, z1, x1, y1, z2],
-        [x2, y1, z1, x2, y2, z2],
-        [x2, y1, z1, x1, y1, z2],
-        [x1, y2, z1, x2, y2, z2],
-        [x2, y1, z1, x1, y2, z1],
-        [x1, y1, z2, x2, y2, z2]
-      ]
+      const faces = {
+        west: [x1, y2, z1, x1, y1, z2],
+        east: [x2, y1, z1, x2, y2, z2],
+        down: [x2, y1, z1, x1, y1, z2],
+        up: [x1, y2, z1, x2, y2, z2],
+        north: [x2, y1, z1, x1, y2, z1],
+        south: [x1, y1, z2, x2, y2, z2]
+      }
 
-      for (const [ax, ay, az, bx, by, bz] of faces) {
+      for (const faceName in element.faces) {
+        const [ax, ay, az, bx, by, bz] = faces[faceName as MinecraftModelFaceName]
+
         vertices.push(bx, ay, az)
         vertices.push(ax, by, az)
         vertices.push(ax, ay, bz)
