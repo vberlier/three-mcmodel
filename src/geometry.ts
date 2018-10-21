97fd64c55a72f966096c4681271ceaac7d461088
Valentin Berlier <berlier.v@gmail.com>
2018-10-21 04:22:09 +0200

    Refactor geometry.ts

--- src/geometry.ts
+++ src/geometry.ts
@@ -15,17 +15,43 @@ const vertexMap: {
   south: [[0, 0, 1], [0, 1, 1], [1, 1, 1], [1, 0, 1]]
 }
 
+function getRotatedVertexMap (rotation: RotationAngle, [a, b, c, d]: FaceVertexMap) {
+  return (
+    rotation === 0 ? [a, b, c, d] :
+    rotation === 90 ? [b, c, d, a] :
+    rotation === 180 ? [c, d, a, b] :
+    [d, a, b, c]
+  ) as FaceVertexMap
+}
+
+function getGeneratedUvs (faceName: MinecraftModelFaceName, [x1, y1, z1]: ArrayVector3, [x2, y2, z2]: ArrayVector3) {
+  return (
+    faceName === 'west' ? [z1, 16 - y2, z2, 16 - y1] :
+    faceName === 'east' ? [16 - z2, 16 - y2, 16 - z1, 16 - y1] :
+    faceName === 'down' ? [x1, 16 - z2, x2, 16 - z1] :
+    faceName === 'up' ? [x1, z1, x2, z2] :
+    faceName === 'north' ? [16 - x2, 16 - y2, 16 - x1, 16 - y1] :
+    [x1, 16 - y2, x2, 16 - y1]
+   ) as ArrayVector4
+}
+
+function computeNormalizedUvs (uvs: ArrayVector4) {
+  return uvs.map((coordinate, i) =>
+    (i % 2 ? 16 - coordinate : coordinate) / 16
+  ) as ArrayVector4
+}
+
 export class MinecraftModelGeometry extends BufferGeometry {
   constructor (model: MinecraftModel) {
     super()
-    const [vertices, uvs, indices] = this.computeAttributes(model)
+    const { vertices, uvs, indices } = MinecraftModelGeometry.computeAttributes(model)
 
     this.addAttribute('position', new Float32BufferAttribute(vertices, 3))
     this.addAttribute('uv', new Float32BufferAttribute(uvs, 2))
     this.setIndex(new Uint16BufferAttribute(indices, 1))
   }
 
-  private computeAttributes (model: MinecraftModel) {
+  public static computeAttributes (model: MinecraftModel) {
     const vertices = []
     const uvs = []
     const indices = []
@@ -41,12 +67,12 @@ export class MinecraftModelGeometry extends BufferGeometry {
         indices.push(i, i + 2, i + 1)
         indices.push(i, i + 3, i + 2)
 
-        for (const vertex of this.rotatedVertexMap(face.rotation || 0, vertexMap[faceName])) {
+        for (const vertex of getRotatedVertexMap(face.rotation || 0, vertexMap[faceName])) {
           vertices.push(...vertex.map((v, i) => v === 0 ? from[i] : to[i]))
         }
 
-        const faceUvs = face.uv || this.generatedUvs(faceName, from, to)
-        const [u1, v1, u2, v2] = this.computeNormalizedUvs(faceUvs)
+        const faceUvs = face.uv || getGeneratedUvs(faceName, from, to)
+        const [u1, v1, u2, v2] = computeNormalizedUvs(faceUvs)
 
         uvs.push(u1, v2)
         uvs.push(u1, v1)
@@ -55,32 +81,6 @@ export class MinecraftModelGeometry extends BufferGeometry {
       }
     }
 
-    return [vertices, uvs, indices]
-  }
-
-  private rotatedVertexMap (rotation: RotationAngle, [a, b, c, d]: FaceVertexMap): FaceVertexMap {
-    return (
-      rotation === 0 ? [a, b, c, d] :
-      rotation === 90 ? [b, c, d, a] :
-      rotation === 180 ? [c, d, a, b] :
-      [d, a, b, c]
-    )
-  }
-
-  private generatedUvs (faceName: MinecraftModelFaceName, [x1, y1, z1]: ArrayVector3, [x2, y2, z2]: ArrayVector3): ArrayVector4 {
-    return (
-      faceName === 'west' ? [z1, 16 - y2, z2, 16 - y1] :
-      faceName === 'east' ? [16 - z2, 16 - y2, 16 - z1, 16 - y1] :
-      faceName === 'down' ? [x1, 16 - z2, x2, 16 - z1] :
-      faceName === 'up' ? [x1, z1, x2, z2] :
-      faceName === 'north' ? [16 - x2, 16 - y2, 16 - x1, 16 - y1] :
-      [x1, 16 - y2, x2, 16 - y1]
-     ) as ArrayVector4
-  }
-
-  private computeNormalizedUvs (uvs: ArrayVector4): ArrayVector4 {
-    return uvs.map((coordinate, i) =>
-      (i % 2 ? 16 - coordinate : coordinate) / 16
-    ) as ArrayVector4
+    return { vertices, uvs, indices }
   }
 }
