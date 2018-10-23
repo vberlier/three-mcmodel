d102a28d000c3f3a434f738e6f480eac71cd49d6
Valentin Berlier <berlier.v@gmail.com>
2018-10-24 01:17:22 +0200

    Separate vertices linked to different textures into different groups

--- src/geometry.ts
+++ src/geometry.ts
@@ -107,22 +107,73 @@ function computeNormalizedUvs (uvs: ArrayVector4) {
   ) as ArrayVector4
 }
 
+interface GroupAttributes {
+  vertices: number[]
+  uvs: number[]
+  indices: number[]
+}
+
+class GroupedAttributesBuilder {
+  private groups: { [path: string]: GroupAttributes } = {}
+  private groupMapping: { [variable: string]: GroupAttributes } = {}
+  private missingGroup: GroupAttributes = { vertices: [], uvs: [], indices: [] }
+
+  constructor (textures: { [name: string]: string }) {
+    for (const texturePath of new Set(Object.values(textures))) {
+      this.groups[texturePath] = { vertices: [], uvs: [], indices: [] }
+    }
+
+    for (const variable in textures) {
+      this.groupMapping['#' + variable] = this.groups[textures[variable]]
+    }
+  }
+
+  public getContext (textureVariable: string) {
+    return this.groupMapping[textureVariable] || this.missingGroup
+  }
+
+  public getAttributes () {
+    let { vertices, uvs, indices } = this.missingGroup
+    let indexCount = indices.length
+
+    const groups = [{ start: 0, count: indexCount, materialIndex: 0 }]
+
+    groups.push(...Object.keys(this.groups).sort().map((path, i) => {
+      const group = this.groups[path]
+
+      const start = indexCount
+      const count = group.indices.length
+      const offset = vertices.length / 3
+
+      vertices = vertices.concat(group.vertices)
+      uvs = uvs.concat(group.uvs)
+      indices = indices.concat(group.indices.map(index => index + offset))
+
+      indexCount += count
+
+      return { start, count, materialIndex: i + 1 }
+    }))
+
+    return { vertices, uvs, indices, groups }
+  }
+}
+
 export class MinecraftModelGeometry extends BufferGeometry {
   constructor (model: MinecraftModel) {
     super()
-    const { vertices, uvs, indices } = MinecraftModelGeometry.computeAttributes(model)
+    const { vertices, uvs, indices, groups } = MinecraftModelGeometry.computeAttributes(model)
 
     this.addAttribute('position', new Float32BufferAttribute(vertices, 3))
     this.addAttribute('uv', new Float32BufferAttribute(uvs, 2))
     this.setIndex(new Uint16BufferAttribute(indices, 1))
 
-    this.addGroup(0, indices.length, 0)
+    for (const { start, count, materialIndex } of groups) {
+      this.addGroup(start, count, materialIndex)
+    }
   }
 
   public static computeAttributes (model: MinecraftModel) {
-    const vertices = []
-    const uvs = []
-    const indices = []
+    const builder = new GroupedAttributesBuilder(model.textures)
 
     for (const element of model.elements) {
       const { from, to, rotation } = element
@@ -132,6 +183,8 @@ export class MinecraftModelGeometry extends BufferGeometry {
       for (const name in element.faces) {
         const faceName = name as MinecraftModelFaceName
         const face = element.faces[faceName]
+
+        const { vertices, uvs, indices } = builder.getContext(face.texture)
 
         const i = vertices.length / 3
         indices.push(i, i + 2, i + 1)
@@ -151,6 +204,6 @@ export class MinecraftModelGeometry extends BufferGeometry {
       }
     }
 
-    return { vertices, uvs, indices }
+    return builder.getAttributes()
   }
 }
