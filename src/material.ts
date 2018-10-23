ad33f2bfb071cbab7d13a2ea2e06aa5b00692b7c
Valentin Berlier <berlier.v@gmail.com>
2018-10-23 23:47:58 +0200

    Create a material per texture

--- src/material.ts
+++ src/material.ts
@@ -1,10 +1,21 @@
-import { MeshBasicMaterial } from 'three'
+import { MeshBasicMaterial, Texture } from 'three'
 
-import { missingTextureMaterial } from './missingTexture'
+import { missingTexture, missingTextureMaterial } from './missingTexture'
 import { MinecraftModel } from './model'
 
 export class MinecraftModelMaterial extends Array<MeshBasicMaterial> {
-  constructor (_model: MinecraftModel) {
+  private materials: { [path: string]: MeshBasicMaterial } = {}
+
+  constructor ({ textures }: MinecraftModel) {
     super(missingTextureMaterial)
+
+    const texturePaths = [...new Set(Object.values(textures))].sort()
+
+    for (const path of texturePaths) {
+      const material = new MeshBasicMaterial({ map: missingTexture })
+      this.materials[path] = material
+
+      this.push(material)
+    }
   }
 }
